c1003a189e1cbf16fd10668bab9e5ce7bcf91b89
zish <zish82@gmail.com>
2016-01-17 11:45:41 +0000

    implemented counter

--- UnitTest/WhenSearching.cs
+++ UnitTest/WhenSearching.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 using WordCounter.Core;
 
 namespace UnitTest
@@ -9,12 +11,34 @@ namespace UnitTest
         [Test]
         public void EmptySearchReturnsNoResult()
         {
-            var searchString = "";
+            const string searchString = "";
             var searcher = new StringCounter();
 
             var values = searcher.Search(searchString);
 
             Assert.That(values.Count, Is.EqualTo(0));
         }
+        
+        [Test]
+        public void CounterReturnsCountedWords()
+        {
+            const string searchString = "how are you, what are you doing";
+            var expectedResult = new Dictionary<string, int>
+            {
+                {"how", 1},
+                {"are", 2},
+                {"you", 2},
+                {"what", 1},
+                {"doing", 1}
+            };
+            var searcher = new StringCounter();
+
+            var values = searcher.Search(searchString);
+
+            Assert.That(values.Count, Is.EqualTo(expectedResult.Count));
+            Assert.IsTrue(expectedResult.All(x => values.Contains(x)));
+        }
+
+
     }
 }
--- WordCounter.Core/StringCounter.cs
+++ WordCounter.Core/StringCounter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WordCounter.Core
 {
@@ -8,8 +10,25 @@ namespace WordCounter.Core
         {
             var result = new Dictionary<string, int>();
 
+            if (string.IsNullOrEmpty(searchString))
+                return result;
+
+            var source = GetSplitString(searchString);
+            var matchQuery = source.Distinct();
+            
+            foreach (var item in matchQuery)
+            {
+                result.Add(item, source.Count(x => x == item));
+            }
 
             return result;
+        }
+
+        private static string[] GetSplitString(string searchString)
+        {
+            string[] source = searchString.Split(new char[] {'.', '?', '!', ' ', ';', ':', ','},
+                StringSplitOptions.RemoveEmptyEntries);
+            return source;
         }
     }
 }
