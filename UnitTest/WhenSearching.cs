b1bd0d723743f8748e1f6aea602d906d7e984a06
zish <zish82@gmail.com>
2016-01-17 10:58:01 +0000

    Added basic Test

---
+++ UnitTest/WhenSearching.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using WordCounter.Core;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class WhenSearching
+    {
+        [Test]
+        public void NothingFound()
+        {
+            var searchString = "";
+            var searcher = new StringCounter();
+
+            var values = searcher.Search(searchString);
+
+            Assert.That(values.Count, Is.EqualTo(0));
+        }
+    }
+}
---
+++ WordCounter.Core/StringCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WordCounter.Core
+{
+    public class StringCounter
+    {
+        public IDictionary<string, int> Search(string searchString)
+        {
+            var result = new Dictionary<string, int>();
+
+
+            return result;
+        }
+    }
+}
