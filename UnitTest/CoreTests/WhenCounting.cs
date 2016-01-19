2fc1b8788cc961a44878f4a2cda66a665f87233a
Zish <zish82@gmail.com>
2016-01-19 15:07:15 +0000

    Added string separator test and refactored

--- UnitTest/CoreTests/WhenCounting.cs
+++ UnitTest/CoreTests/WhenCounting.cs
@@ -13,20 +13,56 @@ namespace UnitTest.CoreTests
         [Test]
         public void EmptySearchReturnsNoResult()
         {
-            const string countString = "";
+            const string sentence = "";
+            var counter = new WordsCounterService();
+            var countedWords = new ObservableCollection<WordCountViewModel>();
+
+            counter.Count(sentence, countedWords);
+
+            Assert.IsEmpty(countedWords);
+        }
+
+        [TestCase(".")]
+        [TestCase("?")]
+        [TestCase("!")]
+        [TestCase(" ")]
+        [TestCase(";")]
+        [TestCase(":")]
+        [TestCase(",")]
+        public void SentencesSplitOnSpecialCharacters(string specialCharacter)
+        {
+            var sentence = string.Format("firstPart{0}secondPart", specialCharacter);
+            var counter = new WordsCounterService();
+            var countedWords = new ObservableCollection<WordCountViewModel>();
+            var wordCountViewModels = new ObservableCollection<WordCountViewModel> { new WordCountViewModel { Word = "firstPart", Count = 1 }, new WordCountViewModel { Word = "secondPart", Count = 1 } };
+
+            counter.Count(sentence, countedWords);
+
+            Assert.That(countedWords.All(x => Contains(wordCountViewModels, x)));
+        }
+
+        [TestCaseSource("CounterServiceReturnsCountedWordsSource")]
+        public void CounterServiceReturnsCountedWords(string sentence, ObservableCollection<WordCountViewModel> expectedResult)
+        {
             var searcher = new WordsCounterService();
             var countedWords = new ObservableCollection<WordCountViewModel>();
 
-            searcher.Count(countString, countedWords);
+            searcher.Count(sentence, countedWords);
 
-            Assert.That(countedWords.Count, Is.EqualTo(0));
+            Assert.That(countedWords.Count, Is.EqualTo(expectedResult.Count));
+            Assert.IsTrue(expectedResult.All(x => Contains(countedWords, x)));
         }
 
-        [Test]
-        public void CounterReturnsCountedWords()
+        protected IEnumerable<TestCaseData> CounterServiceReturnsCountedWordsSource()
         {
-            const string searchString = "this is a statement, and so is this.";
-            var expectedResult = new ObservableCollection<WordCountViewModel>
+            yield return new TestCaseData("this", new ObservableCollection<WordCountViewModel> { new WordCountViewModel { Word = "this", Count = 1}});
+            yield return new TestCaseData("this is a ", new ObservableCollection<WordCountViewModel>
+            {
+                new WordCountViewModel { Word = "this", Count = 1},
+                new WordCountViewModel { Word = "is", Count = 1},
+                new WordCountViewModel { Word = "a", Count = 1}
+            });
+            yield return new TestCaseData("this is a statement, and so is this.", new ObservableCollection<WordCountViewModel>
             {
                 new WordCountViewModel { Word = "this", Count = 2},
                 new WordCountViewModel { Word = "is", Count = 2},
@@ -34,14 +70,7 @@ namespace UnitTest.CoreTests
                 new WordCountViewModel { Word = "statement", Count = 1},
                 new WordCountViewModel { Word = "and", Count = 1},
                 new WordCountViewModel { Word = "so", Count = 1}
-            };
-            var searcher = new WordsCounterService();
-            var countedWords = new ObservableCollection<WordCountViewModel>();
-
-            searcher.Count(searchString, countedWords);
-
-            Assert.That(countedWords.Count, Is.EqualTo(expectedResult.Count));
-            Assert.IsTrue(expectedResult.All(x => Contains(countedWords, x)));
+            });
         }
 
         private static bool Contains(IEnumerable<WordCountViewModel> values, WordCountViewModel x)
